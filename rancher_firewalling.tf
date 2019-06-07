1ae1dd97576c3fe7f4c433a2d7bf46b2a702bfb7
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-06-07 21:06:12 +0200

    Add initial firewal configuration for exoscale security groups

---
+++ rancher_firewalling.tf
@@ -0,0 +1,98 @@
+resource "exoscale_security_group" "rancher_node" {
+  name        = "rancher-node"
+  description = "Security group that applies to all nodes in the Rancher cluster."
+
+  tags = {
+    kind = "rancher"
+  }
+}
+
+resource "exoscale_security_group" "rancher_master" {
+  name        = "rancher-master"
+  description = "Security group that applies to all master nodes in the Rancher cluster."
+
+  tags = {
+    kind = "rancher"
+  }
+}
+
+resource "exoscale_security_group" "rancher_worker" {
+  name        = "rancher-worker"
+  description = "Security group that applies to all worker nodes in the Rancher cluster."
+
+  tags = {
+    kind = "rancher"
+  }
+}
+
+resource "exoscale_security_group_rules" "rancher_master_rules" {
+  security_group_id = "${exoscale_security_group.rancher_master.id}"
+
+  ingress {
+    description              = "master-to-master communication over tcp"
+    protocol                 = "TCP"
+    cidr_list                = ["${hcloud_server.master-01.ipv4_address}", "${hcloud_server.master-02.ipv4_address}", "${hcloud_server.master-03.ipv4_address}"]
+    ports                    = ["2376", "2379", "2380", "6443", "9796", "10250", "30000-32767"]
+    user_security_group_list = ["rancher-master"]
+  }
+
+  ingress {
+    description              = "master-to-master communication over udp"
+    protocol                 = "UDP"
+    cidr_list                = ["${hcloud_server.master-01.ipv4_address}", "${hcloud_server.master-02.ipv4_address}", "${hcloud_server.master-03.ipv4_address}"]
+    ports                    = ["8472", "30000-32767"]
+    user_security_group_list = ["rancher-master"]
+  }
+
+  ingress {
+    description              = "worker-to-master communication over tcp"
+    protocol                 = "TCP"
+    cidr_list                = ["${hcloud_server.worker-fsn.*.ipv4_address}", "${hcloud_server.worker-nbg.*.ipv4_address}", "${hcloud_server.worker-hel.*.ipv4_address}"]
+    ports                    = ["2376", "6443", "9796", "30000-32767"]
+    user_security_group_list = ["rancher-worker"]
+  }
+
+  ingress {
+    description              = "worker-to-master communication over udp"
+    protocol                 = "UDP"
+    cidr_list                = ["${hcloud_server.worker-fsn.*.ipv4_address}", "${hcloud_server.worker-nbg.*.ipv4_address}", "${hcloud_server.worker-hel.*.ipv4_address}"]
+    ports                    = ["8472", "30000-32767"]
+    user_security_group_list = ["rancher-worker"]
+  }
+}
+
+resource "exoscale_security_group_rules" "rancher_worker_rules" {
+  security_group_id = "${exoscale_security_group.rancher_master.id}"
+
+  ingress {
+    description              = "worker-to-worker communication over tcp"
+    protocol                 = "TCP"
+    cidr_list                = ["${hcloud_server.worker-fsn.*.ipv4_address}", "${hcloud_server.worker-nbg.*.ipv4_address}", "${hcloud_server.worker-hel.*.ipv4_address}"]
+    ports                    = ["2376", "9796", "30000-32767"]
+    user_security_group_list = ["rancher-worker"]
+  }
+
+  ingress {
+    description              = "worker-to-worker communication over udp"
+    protocol                 = "UDP"
+    cidr_list                = ["${hcloud_server.worker-fsn.*.ipv4_address}", "${hcloud_server.worker-nbg.*.ipv4_address}", "${hcloud_server.worker-hel.*.ipv4_address}"]
+    ports                    = ["8472", "30000-32767"]
+    user_security_group_list = ["rancher-worker"]
+  }
+
+  ingress {
+    description              = "master-to-worker communication over tcp"
+    protocol                 = "TCP"
+    cidr_list                = ["${hcloud_server.master-01.ipv4_address}", "${hcloud_server.master-02.ipv4_address}", "${hcloud_server.master-03.ipv4_address}"]
+    ports                    = ["2376", "9796", "10250", "30000-32767"]
+    user_security_group_list = ["rancher-master"]
+  }
+
+  ingress {
+    description              = "master-to-worker communication over udp"
+    protocol                 = "UDP"
+    cidr_list                = ["${hcloud_server.master-01.ipv4_address}", "${hcloud_server.master-02.ipv4_address}", "${hcloud_server.master-03.ipv4_address}"]
+    ports                    = ["8472", "30000-32767"]
+    user_security_group_list = ["rancher-master"]
+  }
+}
