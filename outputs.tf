6b589de39bb2ea846e3a59facfb203dc8d469ac8
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-06-08 16:47:08 +0200

    Remove zone id output

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,6 @@ output "aws_route53_record_lb_apps" {
   value = "${aws_route53_record.lb_apps.name}"
 }
 
-output "aws_route53_record_lb_zone_id" {
-  value = "${aws_route53_record.lb_all.zone_id}"
-}
-
 output "cert_manager_access_key" {
   value = "${aws_iam_access_key.cert_manager_credentials.id}"
 }
