49f13a0e0a08af49aa7d896e362ebff918f4c2e8
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-06-08 17:16:34 +0200

    Fix: Use formatlist for master nodes as well

--- rancher_master_nodes.tf
+++ rancher_master_nodes.tf
@@ -4,11 +4,9 @@ resource "aws_route53_record" "master-nodes" {
   type    = "SRV"
   ttl     = "300"
 
-  records = [
-    "${hcloud_server.master-01.ipv4_address}",
-    "${hcloud_server.master-02.ipv4_address}",
-    "${hcloud_server.master-03.ipv4_address}",
-  ]
+  records = ["${formatlist("10 1 443 %s", list(hcloud_server.master-01.ipv4_address,
+                                               hcloud_server.master-02.ipv4_address,
+                                               hcloud_server.master-03.ipv4_address))}"]
 }
 
 # MASTER 1
