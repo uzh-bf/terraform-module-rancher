a3559fcc01177137536a33a520d6a74bb3881a8f
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-07-08 14:10:28 +0200

    Comment SRV records

--- rancher_master_nodes.tf
+++ rancher_master_nodes.tf
@@ -32,14 +32,14 @@ resource "aws_route53_record" "master_nodes" {
   ]
 }
 
-resource "aws_route53_record" "master_nodes_srv" {
-  zone_id = "${var.aws_route53_zone}"
-  name    = "_http._tcp.${var.prefix}master-nodes.${var.base_domain}"
-  type    = "SRV"
-  ttl     = "300"
+# resource "aws_route53_record" "master_nodes_srv" {
+#   zone_id = "${var.aws_route53_zone}"
+#   name    = "_http._tcp.${var.prefix}master-nodes.${var.base_domain}"
+#   type    = "SRV"
+#   ttl     = "300"
 
-  records = ["${concat(
-    formatlist("10 1 80 %s", hcloud_server.master.*.ipv4_address),
-    formatlist("10 1 443 %s", hcloud_server.master.*.ipv4_address)
-  )}"]
-}
+#   records = ["${concat(
+#     formatlist("10 1 80 %s", hcloud_server.master.*.ipv4_address),
+#     formatlist("10 1 443 %s", hcloud_server.master.*.ipv4_address)
+#   )}"]
+# }
